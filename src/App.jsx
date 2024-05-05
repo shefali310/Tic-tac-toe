46cad757eb9a3206a2329bf27693f353cd6aded0
Shefali Panchal <61265165+shefali310@users.noreply.github.com>
2024-05-04 23:16:23 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import Player from "./components/player"
+import GameBoard from "./components/Gameboard";
+import Log from "./components/Log";
+
+import { useState } from "react";
+
+
+
+function App() {
+
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns();
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer=== 'X'} />
+          <Player initialName="Player 2" symbol="O"   isActive={activePlayer=== 'O'} />
+        </ol>
+
+        <GameBoard  onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+       
+      </div>
+
+      <Log/>
+
+    
+    </main>
+
+  );
+}
+
+export default App
